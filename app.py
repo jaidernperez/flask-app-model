0cc7a8fb0a636f48426edb639421ce1da7c6b7e6
Jaider Perez <jaider.perez@usantoto.edu.co>
2021-05-08 16:58:20 -0500

    feat: implement model

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, request, redirect
 
+import os
+
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import HashingVectorizer
+import re
+import pickle
+
 app = Flask(__name__)
+
+stop = stopwords.words('spanish')
 
 
 @app.route('/')
@@ -10,3 +19,35 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
+
+@app.route('/feelings', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('first_app.html', title='Sentimientos')
+    else:
+        feeling = request.form.get("feeling")
+
+        return f"<h1>El sentimiento '{feeling}'' es {get_feeling_type(feeling)} </h1>"
+
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+
+def get_feeling_type(feeling):
+    clf = pickle.load(open(os.path.join('data/twitter_classifier/pkl_objects', 'classifier.pkl'), 'rb'))
+
+    vect = HashingVectorizer(decode_error='ignore',
+                             n_features=2 ** 21,
+                             preprocessor=None,
+                             tokenizer=tokenizer)
+
+    label = {-1: 'Sin sentimiento', 0: 'Neutro', 1: 'Positivo', 2: 'Negativo'}
+
+    text_convert = vect.transform([feeling])
+    return label[clf.predict(text_convert)[0]]
